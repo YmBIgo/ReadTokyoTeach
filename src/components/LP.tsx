945610b7a2cf6fa8a9d50754335650f1099d05f8
Kazuya kurihara <IgYmb0218LP@gmail.com>
2025-12-04 10:27:00 +0900

    fix img

--- src/components/LP.tsx
+++ src/components/LP.tsx
@@ -27,12 +27,12 @@ const LP: React.FC = () => {
                         <p>1 : 新人がOJTを受けても、なかなか一人で成長しない</p>
                     </div>
                     <div className="main-section1-issue-item">
-                        <img src="./6_pair_programming.jpeg"/>
-                            <p>2 : 新人へのコードを書く指示出しが難しい</p>
+                        <img src="./6_pair_programming.jpeg" />
+                        <p>2 : 新人へのコードを書く指示出しが難しい</p>
                     </div>
                     <div className="main-section1-issue-item">
                         <img src="./5_IT_understanding.png" />
-                        <p>3 : 新人がコードを理解してくれない</p>
+                        <p>3 : 新人や未経験者がプロジェクトを漂流する</p>
                     </div>
                     <div className="main-section1-issue-item">
                         <img src="./3_knowledge_transfer.jpg" />
@@ -56,7 +56,7 @@ const LP: React.FC = () => {
                 <div className="main-section3-title">
                     <h3>OJT・引き継ぎDX「Read」</h3>
                     <p>
-                        効果あります？「独学・ペアプロ・AIに聞く」に。
+                        効果あります？「独学・ペアプロ・文書化・AIに聞く」に。
                         <br />
                         OJT・引き継ぎに「教材」という新しい道を。
                     </p>
@@ -77,7 +77,7 @@ const LP: React.FC = () => {
                     <div className="main-section3-explain-item">
                         <span>3</span>
                         <p>コードを</p>
-                        <h4>「簡単に読める！」</h4>
+                        <h4>「クイズで学べる！」</h4>
                         <img src="./icon_read.png" />
                     </div>
                 </div>
@@ -95,8 +95,8 @@ const LP: React.FC = () => {
                                 可視化</span>した実績あり。
                         </div>
                         <div className="main-section3-inner-left-linux-reader">
-                            <img src="./logo-linux-reader.png"/>
-                                <a href="https://linux.tokyo" className="button">linux.tokyo</a>
+                            <img src="./logo-linux-reader.png" />
+                            <a href="https://linux.tokyo" className="button">linux.tokyo</a>
                         </div>
                     </div>
                 </div>
@@ -146,11 +146,11 @@ const LP: React.FC = () => {
                             私たちのソリューションは、コードの関数の流れを他の人に共有できるので、以下の改善ができます。
                             <ol>
                                 <li>
-                                    <span style={{color: "#1ea183", fontWeight: "bold"}}>指示だし</span>では
+                                    <span style={{ color: "#1ea183", fontWeight: "bold" }}>指示だし</span>では
                                     <u>「先輩社員が重要な関数の流れを共有」</u>し、<strong>後輩社員が膨大な量の全コードを読む手間を省ける</strong>
                                 </li>
                                 <li>
-                                    <span style={{color: "#1ea183", fontWeight: "bold"}}>レビュー</span>では
+                                    <span style={{ color: "#1ea183", fontWeight: "bold" }}>レビュー</span>では
                                     <u>「後輩社員が自分が考えた関連する箇所を共有」</u>し、<strong>先輩社員は後輩社員の思考が合っているかを確認できる</strong>
                                 </li>
                             </ol>
@@ -161,24 +161,24 @@ const LP: React.FC = () => {
                     <h4>
                         <span>03</span>
                         <span className="main-section4-title-content">
-                            コードを<strong>「簡単に読める！」</strong><br />
-                            <small>重要なロジックの流れを選んで表示</small>
+                            コードを<strong>「クイズ形式で学べる！」</strong><br />
+                            <small>重要なロジックの流れをクイズ形式で学習</small>
                         </span>
                     </h4>
                     <div>
-                        <h3>設計や俯瞰図のみならず、関数の流れを１つ１つルートの関数から再現することもできます。</h3>
-                        <img className="main-section4-card-image" src="./linux_tokyo_1_read.png" />
+                        <h3>設計や俯瞰図のみならず、関数の流れを１つ１つクイズ形式で学ぶこともできます。</h3>
+                        <img className="main-section4-card-image" src="./linux_tokyo_1_teach.png" />
                         <p>
-                            私たちのソリューションは、設計や俯瞰図だけではなく、コードの詳細も再現できる仕組みを作りました。
+                            私たちのソリューションは、設計や俯瞰図だけではなく、コードの流れをクイズで学べる仕組みを作りました。
                             <br />
-                            木構造にある任意の関数から選べば、以下のことができます。
+                            今の関数から「次に読むべき関数を選ぶ」クイズを出題できます！
                             <ol>
-                                <li>そこに行き着くまでの関数の経路を関数１つ１つを辿って再現</li>
-                                <li>そこに行き着くまでの関数の内容を説明するChatGPT向けのプロンプトを生成</li>
+                                <li>コードの読解の正しい経路をインタラクティブに学べます</li>
+                                <li>クイズも重要な関数の流れに絞られているので、知識を体系的に学べます</li>
                             </ol>
                         </p>
                         <p>
-                            <a className="button" href="https://linux.tokyo/trees/read_4_nvme_queue_rq__nvme_queue_rq_to_writel?id=a5ba70472097b57d324f91ff" target="_blank">Linuxカーネルでの例</a>
+                            <a className="button" href="https://linux.tokyo/explore/open_3_ext4_lookup__ext4_lookup_to_ext4_dx_find_entry" target="_blank">Linuxカーネルでの例</a>
                         </p>
                     </div>
                 </div>
@@ -197,7 +197,7 @@ const LP: React.FC = () => {
                             お問い合わせのメール宛にお問い合わせください。後日担当から連絡いたします。
                             <br />
                             <br />
-                            <a href="mailto:coffeecupjapan@yahoo.co.jp" className="button" style={{marginTop: "10px"}}>お問い合わせ</a>
+                            <a href="mailto:coffeecupjapan@yahoo.co.jp" className="button" style={{ marginTop: "10px" }}>お問い合わせ</a>
                         </p>
                     </div>
                     <div className="main-section5-card">
@@ -212,7 +212,7 @@ const LP: React.FC = () => {
                     <div className="main-section5-card">
                         <div className="main-section5-card-title">3</div>
                         <h4>常駐型開発支援</h4>
-                        <p>週2〜3日程度の常駐型開発支援で、現場のコードの教材を作ります。1週間1万行教材化を目安にみてください。</p>
+                        <p>1ヶ月以内の常駐型開発支援で、現場のコードの教材を作ります。1週間1万行教材化を目安にみてください。</p>
                     </div>
                     <div className="main-section5-card">
                         <div className="main-section5-card-title">4</div>
@@ -227,10 +227,11 @@ const LP: React.FC = () => {
                 OJT・引き継ぎDX「Read」
                 <hr />
                 まずはご相談ください
-                <a href="mailto:coffeecupjapan@yahoo.co.jp" className="big-button" style={{marginTop: "10px"}}>お問い合わせ</a>
+                <a href="mailto:coffeecupjapan@yahoo.co.jp" className="big-button" style={{ marginTop: "10px" }}>お問い合わせ</a>
             </div>
             <hr />
             <footer>
+                All rights reserved, Copyright (C) スプリングデイズ株式会社
                 <br />
                 アイコンには以下を使用しています。<a target="_blank" href="https://icons8.com/icon/58855/visualization-skill">視覚化スキル</a> アイコン by <a target="_blank" href="https://icons8.com">Icons8</a>
             </footer>
